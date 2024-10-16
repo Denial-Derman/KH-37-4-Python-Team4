e226607e8bfe22211b3facdafe3ecdba45eec03b
Anya <annagalicenko2004@gmail.com>
2024-10-16 17:02:35 +0300

    чек 1

--- LabRab4/projectTask.py
+++ LabRab4/projectTask.py
@@ -22,7 +22,33 @@ def upperCase():
     return upperCase
 # Наступний студент має використати swapcase(), rindex(), find()
 
+# Галиченко Анна
+
+def swapCase():
+    swapped_text = text.swapcase()  # Зміна регістру символів на протилежний
+    return swapped_text
+
+def rindexChar(char):
+    try:
+        return text.rindex(char)  # Пошук останньої позиції символу в рядку
+    except ValueError:
+        return f"Символ '{char}' не знайдено в тексті."
+
+def findSubstring(substring):
+    position = text.find(substring)  # Пошук першої позиції підрядка в рядку
+    if position != -1:
+        return f"Підрядок '{substring}' знайдено на позиції {position}."
+    else:
+        return f"Підрядок '{substring}' не знайдено в тексті."
+
 # main
 print("Центрування:\n", cetnerText())
 print("Нижній регістр:\n", lowerCase())
 print("Верхній регістр:\n", upperCase())
+
+# main Галиченко
+print("Текст з зміненим регістром:\n", swapCase())
+print(rindexChar('н'))  # Приклад пошуку останньої позиції символа 'н'
+print(findSubstring('крила'))  # Приклад пошуку підрядка 'крила'
+
+
